64e4d2f3571702d74c8684581c82e80b3110451e
Logan Blangenois <logan.blangenois@live.com>
2023-04-09 20:05:44 +0200

    style: update typo

--- src/components/elements/introduction.tsx
+++ src/components/elements/introduction.tsx
@@ -1,33 +1,33 @@
 const Introduction = () => {
-   return (
-      <div className="min-h-[calc(100vh-133px)] relative overflow-hidden px-10 sm:px-4 flex items-center">
-         <div className="max-w-5xl mx-auto relative z-20">
-            <h1 className="text-black text-center text-4xl sm:text-5xl lg:text-7xl font-bold">
-               Hi ! I'm Logan,
-               <br />
-               Front-end Developper
-            </h1>
+	return (
+		<div className="min-h-[calc(100vh-133px)] relative overflow-hidden px-10 sm:px-4 flex items-center">
+			<div className="max-w-5xl mx-auto relative z-20">
+				<h1 className="text-black text-center text-4xl sm:text-5xl lg:text-7xl font-bold">
+					Hi ! I'm Logan,
+					<br />
+					Front-end Developper
+				</h1>
 
-            <img
-               src="/images/humans.svg"
-               alt="humans"
-               className="w-full h-auto mt-12 max-w-[689px]"
-               width="699"
-               height="299"
-            />
-         </div>
+				<img
+					src="/images/humans.svg"
+					alt="humans"
+					className="w-full h-auto mt-12 mx-auto max-w-[689px]"
+					width="699"
+					height="299"
+				/>
+			</div>
 
-         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[1611px] h-[592px]">
-            <img
-               src="/images/words.svg"
-               alt="Words of stacks"
-               className="w-full h-full object-cover"
-               width="300"
-               height="110"
-            />
-         </div>
-      </div>
-   );
+			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[1611px] h-[592px]">
+				<img
+					src="/images/words.svg"
+					alt="Words of stacks"
+					className="w-full h-full object-cover"
+					width="300"
+					height="110"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Introduction;
