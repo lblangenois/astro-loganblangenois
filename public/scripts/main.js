6a5f896c5d0873e02f9ad43f3728c1fc1faa81b5
Logan Blangenois <logan.blangenois@live.com>
2023-04-09 19:30:06 +0200

    feat: add uses pages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -74,4 +74,12 @@ window.addEventListener("load", () => {
 	handleSmoothScroll();
 	toggleStickyOnScroll();
 	handleMailTo();
+
+	// Add "target=_blank" to all external links inside .prose element
+	document.querySelectorAll(".prose a").forEach((link) => {
+		if (link.hostname !== window.location.hostname) {
+			link.setAttribute("target", "_blank");
+			link.setAttribute("rel", "noopener noreferrer");
+		}
+	});
 });
